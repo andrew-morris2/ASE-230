212ef474cfa2b3510553a4726f01d8d227066a8c
andrew-morris2 <57757752+andrew-morris2@users.noreply.github.com>
2024-08-27 21:48:35 -0400

    Update Andrew_Morris.php

--- Andrew_Morris.php
+++ Andrew_Morris.php
@@ -119,7 +119,7 @@
 									    </div><!--//resume-timeline-item-header-->
 									    <div class="resume-timeline-item-desc">
 										    <p><?="Provide comprehensive IT support and system administration for a diverse range of clients, including government agencies, municipalities, healthcare organizations, non-profits, housing providers, law firms, and real estate firms. Manage and troubleshoot client networks, hardware, and software, ensuring optimal performance and security. Implement best practices in system configuration, user management, and incident resolution to meet the unique needs of each client."?></p>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Achievements:</h4>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?="Achievements:"?></h4>
 										    <p><?='As a member of a Managed Service Provider, our main focus is ensuring efficiency and uptime while fulfilling client needs. Some achievements I have had in my time as a help desk technician are: ';?></p>
 										    <ul>
 											    <li><?="Provided exceptional technical support to a wide range of clients, ensuring timely and effective resolution of IT issues and maintaining strong client relationships."?></li>
@@ -128,7 +128,7 @@
 												<li><?="Implemented security best practices to harden systems, reducing vulnerabilities and enhancing the security posture of client environments."?></li>
 												<li><?="Facilitated the construction of networks for new or existing clients through installation and configuration of networking devices onsite"?></li>
 										    </ul>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?="Technologies used:"?></h4>
 										    <ul class="list-inline">
 											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill">
 													<?php
@@ -179,7 +179,7 @@
 									    </div><!--//resume-timeline-item-header-->
 									    <div class="resume-timeline-item-desc">
 										    <p><?="In summary, my role at Jabil involved a comprehensive approach to server debugging, from analyzing log files and troubleshooting using Linux tools to replacing faulty components and addressing issues mostly with PCIe. This required a keen eye for detail and a systematic approach to ensure servers met required standards for AWS. "?></p>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Achievements</h4>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?="Achievements"?></h4>
 											<p><?="As a Debug Technician, my role required an understanding of computer hardware and an ability to troubleshoot issues involving hardware components for AWS AI servers. Some achievements I've earned in my 3-month span at Jabil were: "?></p>
 											<ul>
 												<li><?="Worked with peers to come up with solutions to various problems and challenges that newer AWS AI server models would often come across. "?></li>
@@ -187,7 +187,7 @@
 												<li><?="Analyzed log files meticulously to pinpoint points of failure among new test failures."?></li>
 												<li><?="Identified and resolved issues related to data and network switches that were causing testing failures, significantly increasing the number of servers successfully passing tests."?></li>
 											</ul>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?="Technologies used:"?></h4>
 										    <ul class="list-inline">
 											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?="Linux CLI"?></span></li>
 											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?="Secure Shell"?></span></li>
@@ -206,18 +206,18 @@
 										        <h3 class="resume-position-title font-weight-bold mb-1"><?="Warehouse Selector"?></h3>
 										        <div class="resume-company-name ms-auto"><?="Sygma"?></div>
 										    </div><!--//row-->
-										    <div class="resume-position-time"><?"May 2023 - Sep 2023"?></div>
+										    <div class="resume-position-time"><?="May 2023 - Sep 2023"?></div>
 									    </div><!--//resume-timeline-item-header-->
 									    <div class="resume-timeline-item-desc">
 										    <p><?="As a warehouse selector at Sygma, I played a crucial role in the efficient operation of the company’s warehouse and distribution center. My key responsibilities included the accurate and time selection of products for orders, ensuring our customers received their orders correctly and on time. This role required physical agility, attention to detail and strong organizational skills. "?></p>
-											<h4 class="resume-timeline-item-desc-heading font-weight-bold">Achievements:</h4>
+											<h4 class="resume-timeline-item-desc-heading font-weight-bold"><?="Achievements:"?></h4>
 										    <p><?="Sygma often recognized and rewarded those who excelled in their tasks. In my time at Sygma, I achieved many accomplishments including: "?></p>
 											<ul>
 												<li><?="Proposed locations for products throughout the warehouse that led to improvements in picking efficiency for warehouse selectors. "?></li>
 												<li><?="Maintained a minimum rate of 80 throughout my time as a selector, ensuring efficiency and ability to complete orders in a timely manner."?></li>
 												<li><?="Recognized as a top selector for the month of June picking over 1,000 products/day"?></li>
 											</ul>
-										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold"><?="Technologies used:"?></h4>
 										    <ul class="list-inline">
 											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?="RFID Scanner"?></span></li>
 											    <li class="list-inline-item"><span class="badge bg-secondary badge-pill"><?="Bluetooth thin client terminal"?></span></li>
@@ -239,7 +239,7 @@
 				    </div>
 				    <div class="col-lg-3">
 					    <section class="resume-section skills-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Skills &amp; Tools</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?="Skills"?> &amp; Tools</h2>
 						    <div class="resume-section-content">
 						        <div class="resume-skill-item">
 							        <ul class="list-unstyled mb-4">
@@ -276,7 +276,7 @@
 							        </ul>
 						        </div><!--//resume-skill-item-->
 						        <div class="resume-skill-item">
-						            <h4 class="resume-skills-cat font-weight-bold">Others</h4>
+						            <h4 class="resume-skills-cat font-weight-bold"><?="Others"?></h4>
 						            <ul class="list-inline">
 							            <li class="list-inline-item"><span class="badge badge-light"><?="Governance Risk and Compliance"?></span></li>
 							            <li class="list-inline-item"><span class="badge badge-light"><?="Python"?></span></li>
@@ -293,7 +293,7 @@
 						    </div><!--resume-section-content-->
 					    </section><!--//skills-section-->
 					    <section class="resume-section education-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?="Education"?></h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
 								    <li class="mb-2">
@@ -332,16 +332,16 @@
 						    </div>
 					    </section><!--//interests-section-->
 					    <section class="resume-section language-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?="Languages"?></h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled resume-lang-list">
-								    <li class="mb-2"><span class="resume-lang-name font-weight-bold">English</span> <small class="text-muted font-weight-normal">(Native)</small></li>
-								    <li><span class="resume-lang-name font-weight-bold">Spanish</span> <small class="text-muted font-weight-normal">(Non-Professional/Beginner)</small></li>
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold"><?="English"?></span> <small class="text-muted font-weight-normal">(Native)</small></li>
+								    <li><span class="resume-lang-name font-weight-bold"><?="Spanish"?></span> <small class="text-muted font-weight-normal">(Non-Professional/Beginner)</small></li>
 							    </ul>
 						    </div>
 					    </section><!--//language-section-->
 					    <section class="resume-section interests-section mb-5">
-						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?="Interests"?></h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled">
 								    <li class="mb-1"><?="Lifting weights"?></li>
@@ -354,15 +354,15 @@
 				    </div>
 			    </div><!--//row-->
 				<section class="resume-section experience-section mb-5">
-					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Projects</h2>
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3"><?="Projects"?></h2>
 					<div class="row mt-4">
 						<div class="col-md-4">
 							<div class="card">
 								<img src="assets/images/risk.jpeg" alt="Project 1" class="card-img-top" style="height: 200px;">
 								<div class="card-body">
-									<h5 class="card-title">Project 1</h5>
+									<h5 class="card-title"><?="Project 1"?></h5>
 									<p class="card-text"><?="Risk management plan I have designed and written"?></p>
-									<a href="assets/projects/project1.pdf" target="_blank">Go to project</a>
+									<a href="assets/projects/project1.pdf" target="_blank"><?="Go to project"?></a>
 								</div>
 							</div>
 						</div>
@@ -370,9 +370,9 @@
 							<div class="card">
 								<img src="assets/images/python.jpg" alt="Project 2" class="card-img-top" style="height: 200px;">
 								<div class="card-body">
-									<h5 class="card-title">Project 2</h5>
+									<h5 class="card-title"><?="Project 2"?></h5>
 									<p class="card-text"><?="Final Group Project utilizing Python to perform a series of tasks"?></p>
-									<a href="assets/projects/project2.py" target="_blank">Go to project</a>
+									<a href="assets/projects/project2.py" target="_blank"><?="Go to project"?></a>
 								</div>
 							</div>
 						</div>
@@ -380,9 +380,9 @@
 							<div class="card">
 								<img src="assets/images/html.png" alt="Project 3" class="card-img-top" style="height: 190px;">
 								<div class="card-body">
-									<h5 class="card-title">Project 3</h5>
+									<h5 class="card-title"><?="Project 3"?></h5>
 									<p class="card-text"><?="New homepage designed for System Support Associates (still in development)"?></p>
-									<a href="assets/projects/project3/website.html" target="_blank">Go to project</a>
+									<a href="assets/projects/project3/website.html" target="_blank"><?="Go to project"?></a>
 								</div>
 							</div>
 						</div>
@@ -397,7 +397,7 @@
     
     <footer class="footer text-center pt-2 pb-5">
 	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i><?"by Andrew Morris"?>/small>
+        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i><?="by Andrew Morris"?>/small>
     </footer>
 
     
